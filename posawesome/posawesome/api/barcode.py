95844c2dd8348c935a8c294a7810af054d968cd9
shallof <ben97shallof@gmail.com>
2024-12-14 08:34:28 +0200

    Barcode

--- posawesome/posawesome/api/barcode.py
+++ posawesome/posawesome/api/barcode.py
@@ -17,7 +17,7 @@ def generate_bar_code(barcode_data, item,price=None):
     <div style="text-align: center;margin:auto;max-width:500px;">
     <p style="font-size:32px; font-weight: bold;margin-top:-5px;margin-bottom:-5px;text-align: center; text-wrap: balance;">{1}</p>
     </div>
-    <div style="text-align: center;margin:auto;margin-top:-5px">
+    <div style="text-align: center;margin:auto;margin-top:-5px ; margin-bottom:-5px">
     <img  style="align:center" src="data:image/png;base64,{0}" alt="Barcode">
     </div>
     <div style="text-align: center;margin:auto;max-width:500px;">
