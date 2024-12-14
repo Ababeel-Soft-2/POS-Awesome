c5b02b8afca7e10116ab7e4df5313603f8f3d83f
shallof <ben97shallof@gmail.com>
2024-12-14 08:27:19 +0200

    Update Brcode

--- posawesome/posawesome/api/barcode.py
+++ posawesome/posawesome/api/barcode.py
@@ -6,7 +6,7 @@ from barcode.writer import ImageWriter
 import base64
 
 @frappe.whitelist()
-def generate_bar_code(barcode_data, item):
+def generate_bar_code(barcode_data, item,price=None):
     ean = barcode.get_barcode_class('ean')
     barcode_instance = ean(barcode_data, writer=ImageWriter())
     barcode_path = BytesIO()
@@ -14,12 +14,22 @@ def generate_bar_code(barcode_data, item):
     barcode_base64 = base64.b64encode(barcode_path.getvalue()).decode()
 
     html_content = """
-    <div style="text-align: center;margin:auto;max-width:500px;">
-    <p style="font-size:32px; font-weight: bold;margin-top:-5px;margin-bottom:-5px;text-align: center; text-wrap: balance;">{1}</p>
-    </div>
-    <div style="text-align: center;margin:auto;margin-top:-5px">
-     <img  style="align:center" src="data:image/png;base64,{0}" alt="Barcode">
-     </div>
-    """.format(barcode_base64, item)  
+        <div style="text-align: center;margin:auto;max-width:500px;">
+        <p style="font-size:32px; font-weight: bold;margin-top:-5px;margin-bottom:-5px;text-align: center; text-wrap: balance;">{1}</p>
+        </div>
+        <div style="text-align: center;margin:auto;margin-top:-5px">
+        <img  style="align:center" src="data:image/png;base64,{0}" alt="Barcode">
+        </div>
+    """
+    
+    if price:
+        html_content+="""
+        <div style="text-align: center;margin:auto;max-width:500px;">
+        <p style="font-size:32px; font-weight: bold;margin-top:-5px;margin-bottom:-5px;text-align: center; text-wrap: balance;">{2}</p>
+        </div>
+        """
+        html_content.format(barcode_base64, item,price)
+    else:
+        html_content.format(barcode_base64, item)
 
     return html_content
